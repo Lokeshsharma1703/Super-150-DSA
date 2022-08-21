8012038597c238d4ef8625c1174110de330a810d
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-08-21 18:39:42 +0530

    initial commit

---
+++ src/main/java/Assignment1/LCM.java
@@ -0,0 +1,38 @@
+//Take the following as input.
+//
+//        A number (N1)
+//        A number (N2)
+//        Write a function which returns the LCM of N1 and N2. Print the value returned.
+//
+//        Input Format
+//        Constraints
+//        0 < N1 < 1000000000 0 < N2 < 1000000000
+//
+//        Output Format
+//        Sample Input
+//        4
+//        6
+//        Sample Output
+//        12
+//        Explanation
+//        The smallest number that is divisible by both N1 and N2 is called the LCM of N1 and N2.
+
+package Assignment1;
+
+import java.util.Scanner;
+
+public class LCM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int res = 0;
+        while(true){
+            res+=b;
+            if(res%a==0){
+                System.out.println(res);
+                break;
+            }
+        }
+    }
+}
