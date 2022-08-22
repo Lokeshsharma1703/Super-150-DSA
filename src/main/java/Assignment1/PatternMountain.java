6bbc8ce0667218a54c5c65eed7a51a3bea135f49
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-08-22 17:40:59 +0530

    initial commit

--- src/main/java/Assignment1/PatternMountain.java
+++ src/main/java/Assignment1/PatternMountain.java
@@ -1,3 +1,22 @@
+//Take N (number of rows), print the following pattern (for N = 4).
+//
+//		1           1
+//		1 2       2 1
+//		1 2 3   3 2 1
+//		1 2 3 4 3 2 1
+//		Input Format
+//		Constraints
+//		0 < N < 10
+//
+//		Output Format
+//		Sample Input
+//		4
+//		Sample Output
+//		1						1
+//		1	2				2	1
+//		1	2	3		3	2	1
+//		1	2	3	4	3	2	1
+
 package Assignment1;
 import java.util.Scanner;
 
@@ -6,22 +25,31 @@ public class PatternMountain
 	public static void main(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
-		int rows = scanner.nextInt();
-		for (int i = 1; i <= rows; i++)
-		{
-			for (int j = 1; j <= i; j++)
-			{
-				System.out.print(j);
+		int n = scanner.nextInt();
+		int row = 1;
+		int start = 1;
+		int space = ((n-1)*2)-1;
+		while(row<=n){
+			int curr = 1;
+			int i=1;
+			while(i<=row){
+				System.out.print(curr+" ");
+				curr++;
+				i++;
 			}
-
-			for (int j= i*2 ; j < rows*2; j++)
-			{
-				System.out.print(" ");
+			int j=1;
+			while(j<=space){
+				System.out.print("  ");
+				j++;
 			}
-			for (int l = i; l >= 1; l--)
-			{
-				System.out.print(l);
+			if(row==n){
+				curr--;
 			}
+			while(curr>1){
+				System.out.print(--curr +" ");
+			}
+			row++;
+			space-=2;
 			System.out.println();
 		}
 	}
