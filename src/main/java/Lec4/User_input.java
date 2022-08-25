4bcb64aacf977e6042b72f887a8ebceea206473c
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-08-25 19:07:26 +0530

    initial commit

---
+++ src/main/java/Lec4/User_input.java
@@ -0,0 +1,20 @@
+package Lec4;
+
+import java.util.Scanner;
+
+public class User_input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        display(arr);
+    }
+    public static void display(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
