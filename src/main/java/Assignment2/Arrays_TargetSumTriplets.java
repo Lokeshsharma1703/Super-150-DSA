7ebe0e658c65b4a9803911f13a765e615e2dc6b6
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-09-04 15:48:56 +0530

    initial commit

--- src/main/java/Assignment2/Arrays_TargetSumTriplets.java
+++ src/main/java/Assignment2/Arrays_TargetSumTriplets.java
@@ -51,7 +51,7 @@ public class Arrays_TargetSumTriplets {
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
                 for(int k=j+1;k<arr.length;k++){
-                    if((arr[i]+arr[j]+arr[k]==target) && ((arr[i]!=arr[j])&&(arr[j]!=arr[k])&&(arr[i]!=arr[k])))
+                    if(arr[i]+arr[j]+arr[k]==target)
                         System.out.println(arr[i]+", "+arr[j]+" and "+arr[k]);
                 }
             }
