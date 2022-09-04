b1f0d336bed47006ed4aef5da69eb06c3c01673b
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-09-04 20:06:35 +0530

    initial commit

---
+++ src/main/java/Assignment2/InverseOfAnArray.java
@@ -0,0 +1,48 @@
+//Take as input N, a number. Take N more inputs and store that in an array. Write a recursive function which inverses the array. Print the values of inverted array
+//
+//        Input Format
+//        Enter a number N and take N more inputs
+//
+//        Constraints
+//        None
+//
+//        Output Format
+//        Display the values of the inverted array in a space separated manner
+//
+//        Sample Input
+//        5
+//        0 2 4 1 3
+//        Sample Output
+//        0 3 1 4 2
+//        Explanation
+//        Swap element with index
+//
+//        for eg : element 4 at index 2 becomes element 2 at index 4
+
+package Assignment2;
+
+import java.util.Scanner;
+
+public class InverseOfAnArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+            arr[i] = sc.nextInt();
+        inverse(arr);
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+    private static void inverse(int[] arr){
+        int n = arr.length;
+        int[] temp = new int[n];
+        for(int i=0;i<n;i++){
+            temp[arr[i]] = i;
+        }
+        for(int i=0;i<n;i++){
+            arr[i] = temp[i];
+        }
+    }
+}
