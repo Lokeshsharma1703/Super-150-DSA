2625d70e46980717f539ec2f5fcde53ec1cbb80a
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-09-08 19:40:42 +0530

    initial commit

--- src/main/java/Lec8/PalindromicString.java
+++ src/main/java/Lec8/PalindromicString.java
@@ -1,12 +1,22 @@
-//package Lec8;
-//
-//import java.util.Scanner;
-//
-//public class PalindromicString {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        String s = sc.next();
-//
-//    }
-//    private static
-//}
+package Lec8;
+
+import java.util.Scanner;
+
+public class PalindromicString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(checkPalindrome(s));
+    }
+    private static boolean checkPalindrome(String s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
