cdf85513c356eb14078dcdb2afdc3bb7f5c331b3
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-09-08 19:43:03 +0530

    initial commit

---
+++ src/main/java/Lec8/String_demo.java
@@ -0,0 +1,20 @@
+package Lec8;
+
+public class String_demo {
+    public static void main(String[] args) {
+        String s = "Hello";
+        String s1 = new String("Hello");
+        String s2 = "Hello";
+//        System.out.println(s);
+//        System.out.println(s1);
+//        System.out.println(s2);
+//        System.out.println(s==s1);
+//        System.out.println(s==s2);
+        String s3 = "Hello"+"Bye";
+        String s4 = "Hello"+"Bye";
+        s = s+"bye";
+//        System.out.println(s3==s4);
+//        System.out.println(s4==s);
+
+    }
+}
