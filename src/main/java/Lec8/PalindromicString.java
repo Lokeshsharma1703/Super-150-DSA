22dea23b4201f166ee8aeb61ece1e6531da6a44e
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-09-08 19:37:21 +0530

    initial commit

---
+++ src/main/java/Lec8/PalindromicString.java
@@ -0,0 +1,12 @@
+//package Lec8;
+//
+//import java.util.Scanner;
+//
+//public class PalindromicString {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String s = sc.next();
+//
+//    }
+//    private static
+//}
