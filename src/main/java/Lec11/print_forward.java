d499b56122408692dc1bbd60ffb25d5aafabe7c4
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-09-21 14:33:42 +0530

    initial commit

---
+++ src/main/java/Lec11/print_forward.java
@@ -0,0 +1,15 @@
+package Lec11;
+
+public class print_forward {
+    public static void main(String[] args) {
+        print(5);
+    }
+    private static void print(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        print(n-1);
+        System.out.println(n);
+    }
+}
