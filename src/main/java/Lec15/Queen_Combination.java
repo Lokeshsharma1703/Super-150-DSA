6c49dd94a45ceadbbb869f951e246c8f67081f12
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-10-12 18:55:37 +0530

    Backtracking

---
+++ src/main/java/Lec15/Queen_Combination.java
@@ -0,0 +1,27 @@
+package Lec15;
+
+import java.util.Scanner;
+
+public class Queen_Combination {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean[] board = new boolean[n];
+        int tq = 2;
+        Printanswer(board, tq, 0, "", 0);
+    }
+    //qpsf(queen place so far)
+    private static void Printanswer(boolean[] board, int tq, int qpsf, String res, int index){
+        if(qpsf==tq){
+            System.out.println(res);
+            return;
+        }
+        for(int i=index;i<board.length;i++){
+            if(board[i]==false){
+                board[i] = true;
+                Printanswer(board, tq, qpsf+1, res+"b"+i+"q"+qpsf, i+1);
+                board[i] = false; //undo step / backtrack step
+            }
+        }
+    }
+}
