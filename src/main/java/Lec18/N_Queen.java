1e69860cd164d9d48a24ec0c63b17f57add9b6a8
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-10-20 18:45:34 +0530

    initial commit

---
+++ src/main/java/Lec18/N_Queen.java
@@ -0,0 +1,67 @@
+package Lec18;
+
+import java.util.*;
+
+public class N_Queen {
+    public static void main(String[] args) {
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        List<List<String>> res = new ArrayList<>();
+        queenProblem(board, n, 0);
+    }
+    private static void queenProblem(boolean[][] board, int tq, int row){
+        if(tq==0){
+            display(board);
+            return;
+        }
+        for(int col=0;col<board.length;col++){
+            if(isSafe(board, row, col)){
+                board[row][col] = true;
+                queenProblem(board, tq-1, row+1);
+                board[row][col] = false;
+            }
+        }
+    }
+    private static void display(boolean[][] board){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    private static boolean isSafe(boolean[][] board, int row, int col){
+        int r = row;
+        //vertical
+        while (r>=0){
+            if(board[r][col]==true){
+                return false;
+            }
+            r--;
+        }
+
+        //left diagonal
+        r = row;
+        int c = col;
+        while (r>=0 && c>=0){
+            if(board[r][c]==true){
+                return false;
+            }
+            r--;
+            c--;
+        }
+
+        //right diagonal
+        r = row;
+        c = col;
+        while (r>=0 && c<board.length){
+            if(board[r][c]==true){
+                return false;
+            }
+            r--;
+            c++;
+        }
+        return true;
+    }
+}
