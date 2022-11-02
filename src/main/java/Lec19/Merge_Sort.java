dfe18bc144ac9d53069059a641c262667056c305
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-11-02 19:08:56 +0530

    initial commit

---
+++ src/main/java/Lec19/Merge_Sort.java
@@ -0,0 +1,43 @@
+package Lec19;
+
+public class Merge_Sort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,7,1, -1, 8, -11};
+        int[] res =  mergeSort(arr, 0, arr.length-1);
+        for(int i:res){
+            System.out.print(i+" ");
+        }
+    }
+    private static int[] mergeSort(int[] arr, int si, int ei){
+        if(si==ei){
+            int[] bs = new int[1];
+            bs[0] = arr[si];
+            return bs;
+        }
+        int mid = (si+ei)/2;
+        int[] left = mergeSort(arr, si, mid);
+        int[] right = mergeSort(arr, mid+1, ei);
+        return mergeTwoSortedArray(left, right);
+    }
+    private static int[] mergeTwoSortedArray(int[] arr1, int[] arr2){
+        int i = 0;
+        int j=0;
+        int k=0;
+        int[] res = new int[arr1.length+arr2.length];
+        while (i<arr1.length && j<arr2.length){
+            if(arr1[i]<arr2[j]){
+                res[k++] = arr1[i++];
+            }
+            else {
+                res[k++] = arr2[j++];
+            }
+        }
+        while (i<arr1.length){
+            res[k++] = arr1[i++];
+        }
+        while (j<arr2.length){
+            res[k++] = arr2[j++];
+        }
+        return res;
+    }
+}
