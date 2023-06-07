00028082a81fb2a90a6dd9fc1146e54eb39248eb
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-06-07 17:28:34 +0530

    DP Adv.

---
+++ src/main/java/Lec51/Matrix_Chain_Multiplication.java
@@ -0,0 +1,27 @@
+package Lec51;
+
+public class Matrix_Chain_Multiplication {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 3, 5, 1};
+        System.out.println(mcm(arr, 0, arr.length-1));
+    }
+
+    private static int mcm(int[] arr, int si, int ei){
+        if(si+1 == ei){
+            return 0;
+        }
+
+        int res = Integer.MAX_VALUE;
+
+        for(int k = si+1; k<ei; k++){
+            int fs = mcm(arr, si, k);
+            int ss = mcm(arr, k, ei);
+            int rs = arr[si] * arr[k] * arr[ei];
+
+            res = Math.min(res, fs+ss+rs);
+        }
+
+        return res;
+    }
+
+}
